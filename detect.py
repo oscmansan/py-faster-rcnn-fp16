6220e498e3a1bfc4ae7b38b9cd7146abfc7690d2
Oscar Mañas <oscmansan@gmail.com>
2016-07-27 17:26:42 +0200

    Some improvements to detect.py

--- detect.py
+++ detect.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 import caffe, os, sys, cv2
+from os import walk
+import time
+import math
 
 CLASSES = ('__background__',
            'aeroplane', 'bicycle', 'bird', 'boat',
@@ -28,6 +31,7 @@ def vis_detections(ax, class_name, dets, thresh=0.5):
         return
 
     for i in inds:
+    	i = int(i)
         bbox = dets[i, :4]
         score = dets[i, -1]
 
@@ -46,58 +50,61 @@ def vis_detections(ax, class_name, dets, thresh=0.5):
     plt.tight_layout()
 
 
-def detect(net, im_file):
-    # Load image
-    im = cv2.imread(im_file)
-
+def detect(net, im_files):
     # Detect all object classes and regress object bounds
-    N = 10
+    N = len(im_files)
+    sqrtN = int(math.ceil(math.sqrt(N)))
+    fig, axarr = plt.subplots(sqrtN, sqrtN, sharey=True, figsize=(16,12))
+    axarr = axarr.flatten()
+    imarr = []
     avg = 0
-    for i in range(N):
-	    timer = Timer()
-	    timer.tic()
-	    scores, boxes = im_detect(net, im)
-	    timer.toc()
-	    avg += timer.total_time
-	    print ('Iteration {:d} took {:.3f}s').format(i, timer.total_time)
+    for i, im_file in enumerate(im_files):
+	    imarr.append(cv2.imread(im_file))
+	    start = time.clock()
+	    scores, boxes = im_detect(net, imarr[i])
+	    end = time.clock()
+	    diff = end - start
+	    avg += diff
+	    print ('{:s} took {:.3f}s').format(im_file, diff)
+
+	    print 'boxes.spape:', boxes.shape
+	    print 'scores.shape:', scores.shape
+
+	    CONF_THRESH = 0.5
+	    NMS_THRESH = 0.3
+	    
+	    for cls_ind, cls in enumerate(CLASSES[1:]):
+	        cls_ind += 1
+	        cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
+	        cls_scores = scores[:, cls_ind]
+	        dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])).astype(np.float32)
+	        keep = nms(dets, NMS_THRESH)
+	        dets = dets[keep, :]
+	        vis_detections(axarr[i], cls, dets, thresh=CONF_THRESH)
+
+	    imarr[i] = imarr[i][:, :, (2, 1, 0)]
+
     avg /= N
-    print ('\nDetection took {:.3f}s for {:d} object proposals').format(avg, boxes.shape[0])
+    print ('Detection took {:.3f}s\n').format(avg) 
 
-    print 'boxes.spape:', boxes.shape
-    print 'scores.shape:', scores.shape
-
-    CONF_THRESH = 0.8
-    NMS_THRESH = 0.3
-
-    '''
-    fig, ax = plt.subplots(figsize=(12, 12))
-    for cls_ind, cls in enumerate(CLASSES[1:]):
-        cls_ind += 1
-        print 'Searching for class', CLASSES[cls_ind]
-        cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
-        cls_scores = scores[:, cls_ind]
-        dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])).astype(np.float32)
-        keep = nms(dets, NMS_THRESH)
-        dets = dets[keep, :]
-        vis_detections(ax, cls, dets, thresh=CONF_THRESH)
-
-    im = im[:, :, (2, 1, 0)]
-    ax.imshow(im, aspect='equal')
-    '''
+    for i in range(N):
+		axarr[i].imshow(imarr[i], aspect='equal')
 
 
 def usage():
-    print 'Usage:', sys.argv[0], 'image_file'
+    print 'Usage:', sys.argv[0], 'path'
     sys.exit(0)
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2: usage()
+
     cfg.TEST.HAS_RPN = True  # Use RPN for proposals
 
-    #prototxt = os.path.join(MODELS_DIR, 'pascal_voc', 'VGG16', 'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
-    #caffemodel = os.path.join(DATA_DIR, 'faster_rcnn_models', 'VGG16_faster_rcnn_final.caffemodel')
-    prototxt = os.path.join(MODELS_DIR, 'pascal_voc', 'ZF', 'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
-    caffemodel = os.path.join(DATA_DIR, 'faster_rcnn_models', 'ZF_faster_rcnn_final.caffemodel')
+    prototxt = os.path.join(MODELS_DIR, 'pascal_voc', 'VGG16', 'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
+    caffemodel = os.path.join(DATA_DIR, 'faster_rcnn_models', 'VGG16_faster_rcnn_final.caffemodel')
+    #prototxt = os.path.join(MODELS_DIR, 'pascal_voc', 'ZF', 'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
+    #caffemodel = os.path.join(DATA_DIR, 'faster_rcnn_models', 'ZF_faster_rcnn_final.caffemodel')
 
     caffe.set_mode_gpu()
     caffe.set_device(0)
@@ -106,7 +113,10 @@ if __name__ == '__main__':
 
     print '\n\nLoaded network {:s}\n'.format(caffemodel)
 
-    if len(sys.argv) < 2: usage()
-    im_file = sys.argv[1]
-    detect(net, im_file)
+    path = sys.argv[1]
+    for (_, _, im_files) in os.walk(path):
+    	print 'input images:', str(im_files) + '\n'
+    	im_files = map(lambda im: path + '/' + im, im_files)
+
+    detect(net, im_files)
     plt.show()
