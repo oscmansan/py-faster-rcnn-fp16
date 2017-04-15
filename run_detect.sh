832d36249fb5001de6419715d3a33cccd7319677
Oscar Mañas <oscmansan@gmail.com>
2017-04-15 21:20:36 +0200

    Application working with Dtype=float & Mtype=float

--- run_detect.sh
+++ run_detect.sh
@@ -1,1 +1,2 @@
-sudo LD_LIBRARY_PATH=/usr/local/cuda/lib ./detect.py data/images
+export PYTHONPATH=/home/oscar/py-faster-rcnn-fp16/caffe-fast-rcnn/python
+sudo LD_LIBRARY_PATH=/usr/local/cuda/lib:caffe-fast-rcnn/.build_release/lib ./detect.py data/images
